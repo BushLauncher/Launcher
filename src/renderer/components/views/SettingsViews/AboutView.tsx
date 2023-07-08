d3756e6da0947f028d06cea41a548ebbd343bc78
Gagafeee <64372089+Gagafeee@users.noreply.github.com>
2023-07-08 23:35:48 +0200

    Added gatoux to about view

--- src/renderer/components/views/SettingsViews/AboutView.tsx
+++ src/renderer/components/views/SettingsViews/AboutView.tsx
@@ -10,8 +10,8 @@ export default function AboutView() {
           <p>by <a href={'https://github.com/gagafeee'} target={'_blank'}>@Gagafeee</a></p>
         </div>
         <div>
-          <p>Thanks to <a href='https://mc-heads.net' target={'_blank'}>MCHeads</a> for providing Minecraft avatars.
-          </p>
+          <p>Thanks to <a href='https://mc-heads.net' target={'_blank'}>MCHeads</a> for providing Minecraft avatars.</p>
+          <p>Thanks to <a href='https://discordapp.com/users/947269166132514906' target={'_blank'}>@gatoux</a> for providing backgrounds.</p>
         </div>
       </div>
     </div>
