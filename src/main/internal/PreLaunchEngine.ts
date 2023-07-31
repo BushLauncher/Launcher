35d4a563dbe05cb55ef6b95e694baae373c0531b
Gagafeee <64372089+Gagafeee@users.noreply.github.com>
2023-07-31 22:32:35 +0200

    revive Backend

---
+++ src/main/internal/PreLaunchEngine.ts
@@ -0,0 +1,238 @@
+import * as AccountManager from './AuthModule';
+import { getAccessToken } from './AuthModule';
+import {
+  FinishedSubTaskCallback,
+  GameVersion,
+  LaunchOperationType,
+  LaunchTask,
+  LaunchTaskState,
+  PreLaunchError,
+  PreLaunchTasks,
+  ProgressSubTaskCallback,
+  UpdateLaunchTaskCallback
+} from '../../public/GameDataPublic';
+import { getVersionList, MinecraftVersion } from '@xmcl/installer';
+import { InstallGameFiles, VerifyGameFiles } from './GameFileManager';
+import { versionExist } from './VersionManager';
+import { InstallJava, ResolveJavaPath } from './JavaEngine';
+import { net } from 'electron';
+
+export abstract class ResolvedPreLaunchTask {
+  public type!: LaunchOperationType;
+  public baseTask: LaunchTask;
+
+  protected constructor(baseTask: LaunchTask, type: LaunchOperationType) {
+    console.log('Creating task: ' + baseTask.id + '[', baseTask.params, ']');
+    this.baseTask = baseTask;
+    this.type = type;
+  }
+
+  public getId() {
+    return this.baseTask.id;
+  }
+
+  public abstract run(callback: (callback: UpdateLaunchTaskCallback) => void): Promise<FinishedSubTaskCallback>;
+
+  public getCallback(processingCallback: ProgressSubTaskCallback): UpdateLaunchTaskCallback {
+    return {
+      task: this.baseTask,
+      state: LaunchTaskState.processing,
+      displayText: processingCallback.displayText ? processingCallback.displayText : undefined,
+      data: {
+        return: processingCallback.return ? processingCallback.return : undefined,
+        localProgress: processingCallback.localProgress ? processingCallback.localProgress : undefined
+      }
+    };
+  }
+
+}
+
+export class ParseAccount extends ResolvedPreLaunchTask {
+  constructor(baseTask: LaunchTask) {
+    super(baseTask, LaunchOperationType.Parse);
+  }
+
+  public override async run(callback: (callback: UpdateLaunchTaskCallback) => void) {
+    callback({ task: this.baseTask, state: LaunchTaskState.starting, displayText: 'Checking account...' });
+    const data = await parseAccount();
+    return <FinishedSubTaskCallback>{
+      task: this.baseTask,
+      state: LaunchTaskState.finished,
+      data: { return: data },
+      response: { success: typeof data === 'string', data: data }
+    };
+  }
+}
+
+export class ParseJava extends ResolvedPreLaunchTask {
+  constructor(baseTask: LaunchTask) {
+    super(baseTask, LaunchOperationType.Parse);
+  }
+
+  public override async run(callback: (callback: UpdateLaunchTaskCallback) => void) {
+    callback({ task: this.baseTask, state: LaunchTaskState.starting, displayText: 'Checking Java...' });
+    const data = await parseJava((c: ProgressSubTaskCallback) => callback(this.getCallback(c))).catch(err=>{
+      console.error(err)
+      return <FinishedSubTaskCallback>{task: this.baseTask, state: LaunchTaskState.finished, response: { success: false }}
+    })
+    return <FinishedSubTaskCallback>{
+      task: this.baseTask, state: LaunchTaskState.finished, response: { success: true, data: data }
+    };
+  }
+}
+
+export interface ParseGameFileLaunchTask extends LaunchTask {
+  params: { version: GameVersion };
+}
+
+export class ParseGameFile extends ResolvedPreLaunchTask {
+  constructor(baseTask: ParseGameFileLaunchTask) {
+    super(baseTask, LaunchOperationType.Parse);
+  }
+
+  public override async run(callback: (callback: UpdateLaunchTaskCallback) => void) {
+    callback({ task: this.baseTask, state: LaunchTaskState.starting, displayText: 'Checking Minecraft files...' });
+    await parseGameFile(this.baseTask.params.version, (c: ProgressSubTaskCallback) => callback(this.getCallback(c))).catch(err => {
+      console.error(err);
+      return <FinishedSubTaskCallback>{
+        task: this.baseTask,
+        state: LaunchTaskState.error,
+        response: { success: false }
+      };
+    });
+    return <FinishedSubTaskCallback>{
+      task: this.baseTask,
+      state: LaunchTaskState.finished,
+      response: { success: true }
+    };
+  }
+}
+
+export class VerifyGameFile extends ResolvedPreLaunchTask {
+  constructor(baseTask: LaunchTask) {
+    super(baseTask, LaunchOperationType.Verify);
+  }
+
+  public override async run(callback: (callback: UpdateLaunchTaskCallback) => void) {
+    callback({ task: this.baseTask, state: LaunchTaskState.starting, displayText: 'Verifying Minecraft Files...' });
+    const report = await VerifyGameFiles(this.baseTask.params.version).catch(err=>{console.error(err); return <FinishedSubTaskCallback>{ task: this.baseTask, state: LaunchTaskState.error, response: {success: false, data: err} }})
+    return <FinishedSubTaskCallback>{ task: this.baseTask, state: LaunchTaskState.finished, data: { return: report }, response: {success: true, data: report} };
+  }
+}
+
+//Minecraft craft bootstrapper like Forge, Fabric ect...
+export class InstallBootstrap extends ResolvedPreLaunchTask {
+  constructor(baseTask: LaunchTask) {
+    super(baseTask, LaunchOperationType.Install);
+  }
+
+  public override async run(callback: (callback: UpdateLaunchTaskCallback) => void) {
+    callback({ task: this.baseTask, state: LaunchTaskState.starting, displayText: 'Installing Game Bootstrap...' });
+    return sendUnImplementedException(this.baseTask);
+  }
+}
+
+export function sendUnImplementedException(task: LaunchTask): FinishedSubTaskCallback {
+  const message = `Function ${task.id} is not implemented`;
+  return {
+    task: task,
+    displayText: message,
+    state: LaunchTaskState.error,
+    response: <PreLaunchError>{ error: message, success: false }
+  };
+}
+
+export const ResolvePreLaunchTask: (baseTask: LaunchTask | ParseGameFileLaunchTask) => ResolvedPreLaunchTask = (baseTask) => {
+  switch (baseTask.id) {
+    case PreLaunchTasks.ParseAccount:
+      return new ParseAccount(baseTask);
+    case PreLaunchTasks.ParseJava:
+      return new ParseJava(baseTask);
+    case PreLaunchTasks.ParseGameFile:
+      return new ParseGameFile(<ParseGameFileLaunchTask>baseTask);
+    case PreLaunchTasks.VerifyGameFile:
+      return new VerifyGameFile(baseTask);
+    case PreLaunchTasks.InstallBootstrap:
+      return new InstallBootstrap(baseTask);
+    default:
+      throw new Error(`Operation ${baseTask.id} not founded !`);
+  }
+
+};
+
+export function ResolvePreLaunchTaskList(launchOperation: LaunchTask[]): ResolvedPreLaunchTask[] {
+  let taskList: ResolvedPreLaunchTask[] = [];
+  launchOperation.map((task) => {
+    taskList.push(ResolvePreLaunchTask(task));
+  });
+  return taskList;
+}
+
+
+export function parseGameFile(version: GameVersion, callback: (callback: ProgressSubTaskCallback) => void): Promise<void> {
+  return new Promise(async (resolve, reject) => {
+      const Install = () => InstallGameFiles(version, (c) => callback(c))
+        .then(async () => {
+          console.log('Installed Minecraft files !');
+          const report = await VerifyGameFiles(version);
+          if (report !== true) {
+            throw new Error('Cannot install Minecraft File, Minecraft\'s installed game files are corrupted\n' + report);
+          } else {
+            callback({ state: LaunchTaskState.finished });
+            resolve();
+          }
+        })
+        .catch((err) => reject(err));
+
+
+      if (!versionExist(version.id)) await Install();
+      else {
+        //check for corruption
+        callback({ state: LaunchTaskState.processing, displayText: 'Checking Minecraft file...' });
+        const checkResult = await VerifyGameFiles(version /*no Callback needed*/);
+        if (checkResult === true) {
+          callback({ state: LaunchTaskState.finished });
+          resolve();
+        } else {
+          callback({ state: LaunchTaskState.processing, displayText: 'Repairing Minecraft Files...' });
+          await Install();
+        }
+      }
+    }
+  );
+}
+
+/**
+ * @return the access_token or *false* if account not valid
+ */
+export async function parseAccount(): Promise<string | null | false> {
+  const account = AccountManager.getSelectedAccount();
+  if (account != null && AccountManager.isAccountValid(account)) {
+    return net.isOnline() ? (await getAccessToken(account)).access_token : null;
+  } else return false;
+}
+
+/**
+ * @return The (installed | founded) Java executable path
+ * @param callback installation callback
+ */
+export function parseJava(callback: (c: ProgressSubTaskCallback) => void): Promise<string> {
+  return new Promise<string>(async (resolve) => {
+    console.log('Parsing java...');
+    callback({ state: LaunchTaskState.processing, displayText: 'Parsing Java...' });
+    const resolvedJavaPath = await ResolveJavaPath();
+    if (typeof resolvedJavaPath === 'string') {
+      console.log('java paths detected');
+      resolve(resolvedJavaPath);
+    } else resolve(await InstallJava((c) => callback(c)));
+  });
+}
+
+export async function ResolveXmclVersion(version: GameVersion): Promise<MinecraftVersion> {
+  const versionList = await getVersionList();
+  const res = versionList.versions.find((MinecraftVersion) => {
+    return MinecraftVersion.id === version.id;
+  });
+  if (res === undefined) throw new Error('Cannot resolve version from XMCL lib');
+  else return res;
+}
---
+++ src/main/internal/VersionManager.ts
@@ -0,0 +1,166 @@
+import { GameType, GameVersion, getDefaultGameType, getDefaultVersion } from '../../public/GameDataPublic';
+import { MinecraftVersion, MinecraftVersionList } from '@xmcl/installer';
+import { existsSync, readdirSync } from 'fs';
+import path from 'path';
+import { SortMinecraftVersion } from './Utils';
+import { userDataStorage } from '../main';
+import { getLocationRoot } from './Launcher';
+import { net } from 'electron';
+
+const { getVersionList: getXMCLVersionList } = require('@xmcl/installer');
+
+export function getSelectedVersion(): GameVersion | undefined {
+  const storageRes: GameVersion | undefined = userDataStorage.get('version.selected');
+  return (storageRes === undefined) ? getDefaultVersion(getDefaultGameType) : storageRes;
+}
+
+export type getVersionMethode = 'network' | 'local' | 'auto';
+
+export async function getAllVersionList(methode?: getVersionMethode): Promise<GameVersion[]> {
+  if (methode === undefined) methode = 'auto';
+  let res: GameVersion[] = [];
+  for (const versionType of Object.values(GameType)) {
+    if (net.isOnline() && (methode === 'auto' || methode === 'network')) res = res.concat(await getVersionList(versionType));
+    else res = res.concat(getLocalVersionList(versionType));
+  }
+  return res;
+}
+
+export async function getVersionList(gameType: GameType): Promise<GameVersion[]> {
+  return new Promise<GameVersion[]>(async (resolve, reject) => {
+    switch (gameType) {
+      case GameType.VANILLA: {
+        let foundedList: GameVersion[] = [];
+        const versionList = await getXMCLVersionList()
+          .catch((err: any) => {
+            console.log(err);
+            reject('Cannot get minecraft version list' + err);
+          });
+        for (const version of versionList.versions) {
+          //reindexing version list to get only releases
+          if (version.type === 'release') {
+            let newVersion: GameVersion = {
+              id: version.id,
+              gameType: gameType,
+              installed: versionExist(version.id)
+            };
+            foundedList.push(newVersion);
+          }
+        }
+        resolve(foundedList);
+        break;
+      }
+      default:
+        console.error(`[getVersionList]: The gameType: ${gameType} is not implemented`);
+        resolve([]);
+    }
+  });
+}
+
+export function getLocalVersionList(gameType: GameType): GameVersion[] {
+  const localURL = path.join(getLocationRoot(), 'versions');
+  //TODO: make this function use Version.parse from @xmcl/installer
+  switch (gameType) {
+    case GameType.VANILLA: {
+      const regex = /^\b\d+\.\d+(\.\d+)?\b$/g;
+      //get all folder in local appdata
+      if (!existsSync(localURL)) return [];
+      const foldersToProcess = readdirSync(localURL);
+      let minecraftVersionsList: GameVersion[];
+      minecraftVersionsList = foldersToProcess
+        .filter((folder) => regex.test(folder) && isSupported(gameType, folder)).sort(SortMinecraftVersion)
+        .reverse()
+        .map((folder): GameVersion => {
+          const version: GameVersion = constructVersion(gameType, folder, true);
+          version.installed = true;
+          /*got from locals files, so version folder exist*/
+          return version;
+        });
+      return minecraftVersionsList;
+    }
+    default:
+      console.error(`[getLocalVersionList]: The gameType: ${gameType} is not implemented`);
+      return [];
+  }
+}
+
+export function versionExist(versionName: string): boolean {
+  const localURL = getLocationRoot() + '\\versions\\';
+  if (existsSync(localURL)) return readdirSync(localURL).includes(versionName);
+  else return false;
+}
+
+
+function constructVersion(gameType: GameType, id: string, installed?: boolean): GameVersion {
+  return <GameVersion>{ gameType, id, installed };
+}
+
+async function isSupported(gameType: GameType, id: string) {
+  const list: MinecraftVersionList = await getXMCLVersionList();
+  switch (gameType) {
+    case GameType.VANILLA:
+      return list.versions.findIndex(version => version.id === id) !== -1;
+    default:
+      throw new Error('GameType: ' + gameType + ' is not implemented');
+  }
+}
+
+export interface GroupedGameVersions { group: true, parent: GameVersion; children: GameVersion[] };
+
+export function groupMinecraftVersions(versionsList: GameVersion[]): (GameVersion | GroupedGameVersions)[] {
+  versionsList.sort((a, b) => {
+    const aParts = a.id.split('.').map(Number);
+    const bParts = b.id.split('.').map(Number);
+
+    for (let i = 0; i < Math.max(aParts.length, bParts.length); i++) {
+      const aValue = aParts[i] || 0;
+      const bValue = bParts[i] || 0;
+
+      if (aValue !== bValue) {
+        return bValue - aValue;
+      }
+    }
+
+    return 0;
+  });
+
+  const groups: GroupedGameVersions[] = [];
+  let currentGroup: GroupedGameVersions = {
+    parent: versionsList[0],
+    children: [],
+    group: true
+  };
+
+  for (let i = 1; i < versionsList.length; i++) {
+    const version = versionsList[i];
+    const hasCommonParent = hasCommonParentVersion(version, currentGroup.parent);
+
+    if (hasCommonParent) {
+      currentGroup.children.push(version);
+    } else {
+      groups.push(currentGroup);
+      currentGroup = {
+        parent: version,
+        children: [],
+        group: true
+      };
+    }
+  }
+
+  groups.push(currentGroup);
+  return groups.map((group) => group.children.length === 0 ? group.parent : group);
+}
+
+function hasCommonParentVersion(version: GameVersion, parent: GameVersion): boolean {
+  const versionParts = version.id.split('.').map(Number);
+  const parentParts = parent.id.split('.').map(Number);
+
+
+  for (let i = 0; i < versionParts.length - 1; i++) {
+    if (versionParts[i] !== parentParts[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
