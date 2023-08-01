180dc21b254645d07604ff965e4380e4148efe8a
Gagafeee <64372089+Gagafeee@users.noreply.github.com>
2023-08-01 16:43:19 +0200

    Styling internal log

--- src/public/ConsoleManager.ts
+++ src/public/ConsoleManager.ts
@@ -9,6 +9,8 @@ export default class ConsoleManager extends RenderConsoleManager{
   constructor(prefix: string, process: ProcessType) {
     super(prefix,process)
     this.logger = this.isRenderer() ? console : require('electron-log');
+    this.logger.transports.console.format = "[{h}:{i}:{s}] {text}"
+    this.logger.transports.file.format = '[{d}/{m}/{y} {h}:{i}:{s}.{ms}] [{processType}] {text}'
   }
 
   isRenderer() {
